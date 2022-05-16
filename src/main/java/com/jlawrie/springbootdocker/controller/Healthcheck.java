cfc597d4bf8ec5bdca95c1337cc0364a21325f0c
Jason Lawrie <adlib00@yahoo.com>
2022-05-16 16:31:08 -0400

    added second healtcheck method

--- src/main/java/com/jlawrie/springbootdocker/controller/Healthcheck.java
+++ src/main/java/com/jlawrie/springbootdocker/controller/Healthcheck.java
@@ -1,5 +1,6 @@
 package com.jlawrie.springbootdocker.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,9 @@ public class Healthcheck {
     @RequestMapping("/")
     public String healthcheck() {
         return "Success";
+    }
+    @GetMapping("/Test")
+    public String healthcheck2() {
+        return "Successful Test Method";
     }
 }
