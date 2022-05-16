ddefdde050d347316b89f2bc873706c79701279e
Jason Lawrie <adlib00@yahoo.com>
2022-05-16 14:10:03 -0400

    another pipeline tweak

--- src/main/java/com/jlawrie/springbootdocker/controller/Healthcheck.java
+++ src/main/java/com/jlawrie/springbootdocker/controller/Healthcheck.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Healthcheck {
-
-
     @RequestMapping("/")
     public String healthcheck() {
         return "Success";
